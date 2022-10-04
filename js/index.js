eaa5f4be4b4774ca3074c1c1ed841de09f10d03e
dothinh115 <dothinh115@gmail.com>
2022-10-04 21:20:34 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function scrollEvent () {
     var offset = window.pageYOffset;
-    var nav = document.querySelector("nav");
+    var nav = document.querySelector("header");
     if (offset > 500) {
         nav.classList.add("scrollShow")
     }
